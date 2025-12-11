a4aae9ff3dd35ffb4bfb06e5c47286b98fbf4334
John Cipriano <johnmikecip@gmail.com>
2025-12-10 20:02:34 -0800

    more delinting

--- spec/super8_spec.rb
+++ spec/super8_spec.rb
@@ -3,29 +3,29 @@ require "super8"
 RSpec.describe Super8 do
   describe ".config" do
     it "returns a Config instance" do
-      expect(Super8.config).to be_a(Super8::Config)
+      expect(described_class.config).to be_a(Super8::Config)
     end
 
     it "returns the same instance on repeated calls" do
-      expect(Super8.config).to be(Super8.config)
+      expect(described_class.config).to be(described_class.config) # rubocop:disable RSpec/IdenticalEqualityAssertion
     end
   end
 
   describe ".configure" do
     it "yields the config" do
-      expect { |b| Super8.configure(&b) }.to yield_with_args(Super8::Config)
+      expect { |b| described_class.configure(&b) }.to yield_with_args(Super8::Config)
     end
 
     it "allows setting cassette_directory" do
-      Super8.configure { |c| c.cassette_directory = "custom/path" }
-      expect(Super8.config.cassette_directory).to eq("custom/path")
+      described_class.configure { |c| c.cassette_directory = "custom/path" }
+      expect(described_class.config.cassette_directory).to eq("custom/path")
     end
   end
 
   describe ".use_cassette" do
     it "yields to the block" do
       called = false
-      Super8.use_cassette("test") { called = true }
+      described_class.use_cassette("test") { called = true }
       expect(called).to be true
     end
   end
