a546b97cddd91ccdf2727ef82377fffac8455222
John Cipriano <johnmikecip@gmail.com>
2025-12-16 14:38:54 -0800

    store multiple statements per cassette

--- lib/super8/recording_database_wrapper.rb
+++ lib/super8/recording_database_wrapper.rb
@@ -2,18 +2,19 @@ module Super8
   # Wraps an ODBC::Database to intercept method calls for recording.
   # Delegates to the real database and logs interactions to the cassette.
   class RecordingDatabaseWrapper
-    def initialize(real_db, cassette)
+    def initialize(real_db, cassette, connection_id)
       @real_db = real_db
       @cassette = cassette
+      @connection_id = connection_id
       @statement_counter = 0
     end
 
     # Intercept run method to record SQL queries
     def run(sql, *params)
       statement_id = next_statement_id
-      @cassette.record(:run, sql: sql, params: params, statement_id: statement_id)
+      @cassette.record(:run, connection_id: @connection_id, sql: sql, params: params, statement_id: statement_id)
       real_statement = @real_db.run(sql, *params)
-      RecordingStatementWrapper.new(real_statement, statement_id, @cassette)
+      RecordingStatementWrapper.new(real_statement, statement_id, @cassette, @connection_id)
     end
 
     # Delegate all other method calls to the real database for now.
@@ -30,9 +31,8 @@ module Super8
     private
 
     def next_statement_id
-      id = "stmt_#{@statement_counter}"
       @statement_counter += 1
-      id
+      @statement_counter
     end
   end
 end
